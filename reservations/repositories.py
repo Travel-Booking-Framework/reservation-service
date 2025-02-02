5848840d2eaa1255d301611eda8c789dea1bd2e5
alireza-hme <hme.alireza@gmail.com>
2025-02-02 11:39:15 +0330

    Add create & cancle repository classes

---
+++ reservations/repositories.py
@@ -0,0 +1,17 @@
+from .models import Reservation
+
+class ReservationRepository:
+    @staticmethod
+    def create_reservation(user_id, reservation_type, reservation_id):
+        return Reservation.objects.create(
+            user_id=user_id,
+            reservation_type=reservation_type,
+            reservation_id=reservation_id,
+        )
+
+    @staticmethod
+    def cancel_reservation(reservation_id):
+        reservation = Reservation.objects.get(id=reservation_id)
+        reservation.status = 'CANCELLED'
+        reservation.save()
+        return reservation
