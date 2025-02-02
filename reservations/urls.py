0d4a5664120bc65cf6e8486e795826867573acb7
alireza-hme <hme.alireza@gmail.com>
2025-02-02 14:13:05 +0330

    Modify urls in reservations

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,14 +1,7 @@
-# reservations/urls.py
 from django.urls import path
 from .views import CreateReservationView, CancelReservationView
 
 urlpatterns = [
     path('reservations/', CreateReservationView.as_view(), name='create-reservation'),
     path('reservations/<int:reservation_id>/cancel/', CancelReservationView.as_view(), name='cancel-reservation'),
-]
-
-# Include in main urls.py
-from django.urls import include
-urlpatterns = [
-    path('api/', include('reservations.urls')),
 ]
