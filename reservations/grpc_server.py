cb87fdf8716ec6e6219863506b780dc9f6981c60
alireza-hme <hme.alireza@gmail.com>
2025-02-02 13:08:46 +0330

    Add grpc_server file

---
+++ reservations/grpc_server.py
@@ -0,0 +1,26 @@
+import grpc
+from concurrent import futures
+import reservation_pb2
+import reservation_pb2_grpc
+from .commands import CreateReservationCommand, CancelReservationCommand
+
+class ReservationService(reservation_pb2_grpc.ReservationServiceServicer):
+    def CreateReservation(self, request, context):
+        command = CreateReservationCommand(request.user_id, request.reservation_type, request.reservation_id)
+        reservation = command.execute()
+        return reservation_pb2.ReservationResponse(id=reservation.id, status=reservation.status)
+
+    def CancelReservation(self, request, context):
+        command = CancelReservationCommand(request.reservation_id)
+        reservation = command.execute()
+        return reservation_pb2.ReservationResponse(id=reservation.id, status=reservation.status)
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    reservation_pb2_grpc.add_ReservationServiceServicer_to_server(ReservationService(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+if __name__ == '__main__':
+    serve()
